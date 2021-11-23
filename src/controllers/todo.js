cdfdd1afdb652118d5e8a15ddcc62dd71cb8ef04
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2021-11-23 10:42:40 +0700

    update

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,24 +1,58 @@
 let todos = [
-    {
-        id: 1,
-        title: "Cuci tangan",
-        isDone: true
-    },
-    {
-        id: 2,
-        title: "Jaga jarak",
-        isDone: false
-    },
-    {
-        id: 3,
-        title: "Gunakan masker",
-        idDone: true
-    }
-]
+  {
+    id: 1,
+    title: 'Cuci tangan',
+    isDone: true,
+  },
+  {
+    id: 2,
+    title: 'Jaga jarak',
+    isDone: false,
+  },
+  {
+    id: 3,
+    title: 'Gunakan masker',
+    idDone: true,
+  },
+];
 
 // Create controller get Todos here
+exports.getTodos = async (req, res) => {
+  try {
+    res.send({
+      status: 'success',
+      data: {
+        todos,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 // Create controller get Todo by received id here
+exports.getTodo = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const index = id - 1;
+    res.send({
+      status: 'success',
+      data: {
+        todo: todos[index],
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 // Create controller add Todo here
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
-
 //instantiate express module here
+const express = require('express');
 
 // Init express router here..
+const router = express.Router();
 
 // Get controller here
+const { getTodos, getTodo } = require('../controllers/todo');
 
 // Create Route here
+router.get('/todos', getTodos);
+router.get('/todo/:id', getTodo);
 
 // Export module router here
+module.exports = router;
